2b9295fb2b52894c033cc489a50e92107b271687
root <shaibulumanyaa@gmail.com>
2022-06-04 03:26:40 -0700

    use printf in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry of the program
+ *
+ * print -c function that print formated string
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
