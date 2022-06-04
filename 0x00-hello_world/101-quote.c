6611bbb10b525085cac34548b940e06a0158f13a
root <shaibulumanyaa@gmail.com>
2022-06-04 05:01:43 -0700

    printf to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point of the program
+ *
+ * fprintf- printf out to standard error
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-        10-19");
+
+	return (0);
+
+}
