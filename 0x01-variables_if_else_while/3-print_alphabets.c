d3708c3fa682463743a2739c0cefba11c8691229
lumanya <shaibulumanya@gmail.com>
2022-06-06 14:24:10 +0300

    putchar printing alphatics

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point of the program
+ * for block printing alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+
+
+}
