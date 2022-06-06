17a96485557ae8fc3cc892a723c62e103c27139d
lumanya <shaibulumanya@gmail.com>
2022-06-06 20:21:15 +0300

    combinations of two digits diffenrent digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main -Entry of the program
+ * for -block printing combinations of two digits
+ *
+ * Return: Always 0(sucess)
+ */
+
+int main(void)
+{
+
+	int num1;
+	int num2 = 49;
+	int tmp2;
+
+	for (num1 = 48; num1 <= 56; num1++)
+	{
+		tmp2 = num2;
+
+		for  (; tmp2 <= 57; tmp2++)
+		{
+			putchar(num1);
+			putchar(tmp2);
+
+			if ( num1 != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+
+		}
+
+		num2++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+
+}
