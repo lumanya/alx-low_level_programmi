bc731ddcd67629f8ae637829aacea6b19374d939
root <shaibulumanyaa@gmail.com>
2022-06-06 03:06:46 -0700

    testing last digit of random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ * if - clause test last digit of random number
+ *
+ * Return: Always 0(sucess)
+ */
+
+int main(void)
+{
+
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n % 10 > 5)
+	{
+
+		printf("%d is %d and is greater than 5\n", n, n % 10);
+
+	} else if (n % 10 == 0)
+	{
+
+		printf("%d is %d and is 0\n", n, n % 10);
+
+	} else
+	{
+
+		printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
+
+	}
+
+	return (0);
+}
