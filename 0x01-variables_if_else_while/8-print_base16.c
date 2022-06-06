3d8eb20ab54ca949da9641d0c31b7b3050c67b9e
lumanya <shaibulumanya@gmail.com>
2022-06-06 17:06:02 +0300

    printing hexadecimal number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * for -block printing alphabets in revervese order'
+ *
+ * Return: Always 0(sucess)
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+
+		putchar(ch);
+
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+
+		putchar(ch);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
