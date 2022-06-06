1f16a7f5c7bac3d058fbf0ba87d38103e7f7ecc0
lumanya <shaibulumanya@gmail.com>
2022-06-06 14:52:17 +0300

    printing single digit number only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * for - block printing single digit number
+ *
+ * Return - Always 0 (succes)\
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d\n", num);
+
+	return(0);
+
+
+}
