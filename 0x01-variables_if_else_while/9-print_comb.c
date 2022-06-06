22dd62f6a64a26c707e6dccf66e34e92cf2e7d4c
lumanya <shaibulumanya@gmail.com>
2022-06-06 18:04:14 +0300

    printing two commbinations of digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point of the program
- * two for block printing tow digit 
+ * two for block printing tow digi
  *
  * Return: Always 0(sucess)
  */
@@ -11,22 +11,13 @@ int main(void)
 {
 
 	int first_digit;
-	int second_digit;
 
-	for (first_digit = 48; first_digit <= 57; first_digit++)
+	for ( first_digit = 48; first_digit <= 57; first_digit++)
 	{
 
-		for ( second_digit = 48; second_digit <= 57; second_digit++)
-		{
-
-			putchar(first_digit);
-			putchar(second_digit);
-			putchar(44);
-			putchar(32);
-
-		}
-
-
+		putchar(first_digit);
+		putchar(44);
+		putchar(32);
 	}
 
 	return (0);
