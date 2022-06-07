b4c65438ed69d32f862579ab23c9a2d290221ead
lumanya <shaibulumanya@gmail.com>
2022-06-07 23:28:26 +0300

    function testing for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that check for alphabetic letters
+ *
+ * Return: 1 if c is letter , lowercase or uppercase. 0 false
+ */
+
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+
+}
