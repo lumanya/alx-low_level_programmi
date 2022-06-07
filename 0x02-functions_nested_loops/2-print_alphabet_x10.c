bb0c0c212142894c7eee5a3fe1436e0ef4d5f3d4
lumanya <shaibulumanya@gmail.com>
2022-06-07 15:46:40 +0300

    function that print alphabets in 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print alphabets in ascending order 10 time
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int ch;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+			_putchar(ch);
+
+		_putchar(10);
+	}
+
+
+
+}
