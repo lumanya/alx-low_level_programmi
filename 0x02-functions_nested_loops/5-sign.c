22e850a03a83ce6a6ea90704766f33e65f02f4e0
lumanya <shaibulumanya@gmail.com>
2022-06-08 08:32:19 +0300

    function that print alphabets in lowercase and new line

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,22 +12,22 @@ int print_sign(int n)
 {
 
 
-	if (n >= 49 && n <= 57)
+	if (n > 0)
 	{
 		_putchar(43);
 
 		return (1);
 	}
 
-	else if (n == 48)
+	else if (n < 0)
 	{
-		_putchar(48);
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 
 	else
 	{
-		_putchar(45);
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
