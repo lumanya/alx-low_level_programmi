adc1487de8fc24d1c7b16d62850d2b5b0c27c154
lumanya <shaibulumanya@gmail.com>
2022-06-08 14:17:07 +0300

    function to add tow number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that add two number and return result
+ * @a: integer to be added
+ * @b: integer to be added
+ *
+ * Return: sum of two number
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
