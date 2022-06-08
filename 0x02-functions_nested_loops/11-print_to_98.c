4632d271b822d4c78e559c601eb3227d93581fb8
lumanya <shaibulumanya@gmail.com>
2022-06-08 23:10:51 +0300

    function to print whole number from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_to_98 - function that print whole number from n to 98
  * @n: staring integer for function to work on
@@ -7,25 +9,23 @@
 void print_to_98(int n)
 {
 	int i;
-	int negative;
 
 	if (n >= 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar('0' + i);
-			_putchar(44);
-			_putchar(32);
+			printf("%d, ", i);
 		}
-
+		printf("\n");
 	}
 
 	if (n < 98 && n >= 0)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar('0' + i);
+			printf("%d, ", i);
 		}
+		printf("\n");
 
 	}
 
@@ -34,15 +34,10 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			if (i < 0)
-			{
-				negative = i * -1;
-				_putchar(45);
-				_putchar('0' + negative);
-			} else
-				_putchar('0' + i);
+			printf("%d, ", i);
 		}
+
+		printf("\n");
 	}
 
-	_putchar(10);
 }
