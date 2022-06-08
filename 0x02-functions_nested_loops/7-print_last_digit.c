426d23a7dee9df72e62b02bb123bb995ca0900d2
lumanya <shaibulumanya@gmail.com>
2022-06-08 10:40:59 +0300

    function that print evry minutes and hours in 24 hrs format

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@ int print_last_digit(int n)
 
 	int last_digit;
 
-	if ( n < 0)
+	if (n < 0)
 		last_digit = ((-1 * n) % 10);
 	else
-		last_digit =(n % 10);
+		last_digit = (n % 10);
 
 	_putchar(48 + last_digit);
 
