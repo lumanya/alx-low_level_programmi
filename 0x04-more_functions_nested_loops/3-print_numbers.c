d78221f3bdd8eb6daf0d6e739ae499fecbeb7d8c
lumanya <shaibulumanya@gmail.com>
2022-06-09 15:33:25 +0300

    function that list all number form 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers form 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		_putchar(ch);
+	_putchar(10);
+
+}
