da3faeec3fb855ef521480c595f588e8f6a265b5
lumanya <shaibulumanya@gmail.com>
2022-06-09 15:44:13 +0300

    function that print all numbers form 0 to 9 except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that print numbers form 0 to 9 except 2 & 4
+ */
+
+void print_most_numbers(void)
+{
+
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		if (ch != 50 || ch != 52)
+			_putchar(ch);
+	}
+	_putchar(10);
+}
