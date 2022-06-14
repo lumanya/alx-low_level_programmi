fb2f890bd1d282bd55b691a43b5286d6b671a5c6
lumanya <shaibulumanya@gmail.com>
2022-06-14 21:59:23 +0300

    function that print lenth of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - function that print length of string.
+ * @s: is a pointer int char type
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+	for (len = 0; *s != '\0'; len++)
+		s++;
+
+	return (len);
+}
