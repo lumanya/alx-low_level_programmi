3b8062b7fd8e3ca204b02e93a7fb06874a9ad5ef
lumanya <shaibulumanya@gmail.com>
2022-06-22 13:12:22 +0300

    print string to stdout followed by new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,16 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-
 	if (*s != '\0')
-		_puts_recursion(s);
-	_putchar('\n');
+	{
+		_putchar(*s);
+		s++;
+		if (*s != '\0')
+			_puts_recursion(s);
+		else
+			_putchar('\n');
+	}
+	else
+		_putchar('\n');
 
 }
