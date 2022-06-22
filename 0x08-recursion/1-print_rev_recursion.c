eaad88a8ee818bc99ca4cac61dab915372f4e06e
lumanya <shaibulumanya@gmail.com>
2022-06-23 00:56:04 +0300

    print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: pointer of char type
+ */
 
 void _print_rev_recursion(char *s)
 {
-
 	if (*s != '\0')
 	{
-		
-	}
-	else
-	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
