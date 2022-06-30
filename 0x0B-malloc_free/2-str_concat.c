e41e724c83815b07e81b96de83d46bece880f837
lumanya <shaibulumanya@gmail.com>
2022-06-30 13:40:27 +0300

    concatenate two string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * str_concat - return ponter of two concatenated string
  * @s1: first string
@@ -12,32 +12,25 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat;
 	int l1 = 0, l2 = 0, index, i;
+	char *nul = "";
 
-	if (s1 != NULL)
-	{
-		while (*(s1 + l1))
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
+
+	while (*(s1 + l1))
 		l1++;
-	}
-	else
-	{
-		l1 = 1;
-	}
-	if (s2 != NULL)
-	{
-		while (*(s2 + l2))
+	while (*(s2 + l2))
 		l2++;
-	}
-	else
-	{
-		l2 = 1;
-	}
+
 	concat = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
 
 	if (concat == 0)
 		return (0);
 
 	for (index = 0; index < l1; index++)
-		*(concat + index) = *s1++;
+		*(concat + index) = *(s1 + index);
 
 	for (i = index; i < (l2 + l1); i++)
 		*(concat + i) = *s2++;
