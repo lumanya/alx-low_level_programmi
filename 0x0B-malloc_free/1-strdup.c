c885f31f3822d6aacc7c144c9889d29cef5cccae
lumanya <shaibulumanya@gmail.com>
2022-06-30 12:46:00 +0300

    retunr pointer to newly aloocated space containing string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to new allocated space in memory containg a copy\
+ * of string given as parameter
+ * @str: string
+ *
+ * Return: Pointer to srting(success), NULL for failure
+ */
+
+char *_strdup(char *str)
+{
+	char *dest;
+	int index, len;
+
+	if (*str == 0)
+		return (NULL);
+	len = 0;
+	while (*(str + len))
+		len++;
+
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (dest == 0)
+		return (NULL);
+
+	for (index = 0; index <= len; index++)
+	{
+		*(dest + index) = *(str + index);
+	}
+	return (dest);
+}
