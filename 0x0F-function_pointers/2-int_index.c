3a494c8bfa1e4644b48763b744af775ee2a903ca
lumanya <shaibulumanya@gmail.com>
2022-07-08 16:57:57 +0300

    search for integer in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -seraches for integer in array
+ * @size: size of array
+ * @array: array
+ * @cmp: function pointer
+ *
+ * Return: index of first element , -1 for no element matches and -1 when \
+ * size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,11 @@
 /**
  * Header file containging fuction prototype
  */
-
+#include <stddef.h>
 #ifndef FUNCTIONS_H
 #define FUNCTION_H
 
+int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
