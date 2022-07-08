f1302cc7d6539407f856508d40ca887a1dfbd4d4
lumanya <shaibulumanya@gmail.com>
2022-07-09 00:19:18 +0300

    search for interger

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 && array != 0 && cmp != 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
+	if (array && cmp && size > 0)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	return (-1);
 }
