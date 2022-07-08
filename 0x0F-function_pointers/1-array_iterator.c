d19b93ba685c4cac11cb47373cf9c5c53fbc225c
lumanya <shaibulumanya@gmail.com>
2022-07-08 23:52:47 +0300

    add if condition for array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (action != 0)
 	{
-		if (size > 0)
+		if (size > 0 && array != 0)
 		{
 			for (i = 0; i < size; i++)
 				action(array[i]);
