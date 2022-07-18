9f678ccacfd9ce83788c00ca52176ccf593a293b
lumanya <shaibulumanya@gmail.com>
2022-07-18 23:37:02 +0300

    define  dog struct data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct doling dogs character
+ * @name: pointer to name of dog
+ * @age: pointer to age
+ * @owner: pointer to owner
+ */
+struct dog
+{
+	char *name;
+	float  age;
+	char *owner;
+};
+#endif
