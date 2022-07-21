62ce858b217a08180789529e530ecdc9036256e1
lumanya <shaibulumanya@gmail.com>
2022-07-21 22:54:07 +0300

    check for for character which is not 1 or 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,30 +9,31 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0, one, i;
-	int l = 0;
 
-	if (b == 0)
-		return (0);
+	int len = 0, i;
+	unsigned int sum = 0;
 
+	if (b == NULL)
+		return (sum);
 
-	while (b[l] != 0)
+	/* find string length */
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	i = 0;
+	while (b[i])
 	{
-		if (b[l] != '0' && b[l] != '1')
+		if ((b[i] != '0') && (b[i] != '1'))
 			return (0);
-		l++;
+
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
 
-	l = l - 1;
-
-	for (i = 0; l >= 0; i++, l--)
-	{
-		if (b[l] == '0')
-			one = 0;
-		else if (b[l] == '1')
-			one = 1;
-		one = one << i;
-		res = res | one;
-	}
-	return (res);
+	return (sum);
 }
