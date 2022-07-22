ddc72ee354c4f47cc332ff9ab405bb5cc0bd0937
lumanya <shaibulumanya@gmail.com>
2022-07-22 03:07:26 +0300

    check  max_bits zise of integer

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,18 +9,17 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 
-	unsigned long int n_copy = n, mask;
-	unsigned int len = 0;
+	int bit;
+	unsigned int max_bits = 0;
 
-	while (n_copy > 0)
-	{
-		len++;
-		n_copy >>= 1;
-	}
-	if (index >= len)
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
 		return (-1);
-	if (index > 0)
-		mask = 1 << index;
-	n = n & mask;
-	return (n >> index);
+
+	/* shift number places right to find bit */
+	bit = ((n >> index) & 1);
+
+	return (bit);
+
 }
