18d8db17186ef6a605652dc0bd92a734139b8a76
lumanya <shaibulumanya@gmail.com>
2022-07-27 17:38:46 +0300

    handle for write erro

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,14 +18,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t n_read, n_wrote;
 
-	char *buffer = malloc(sizeof(char) * letters);
+	char *buffer = malloc(sizeof(char) * letters + 1);
 
 	if (buffer == NULL)
 		return (0);
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 	{
@@ -35,13 +35,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	n_read = read(fd, buffer, letters);
 
-	n_wrote = write(STDOUT_FILENO, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	buffer[letters] = '\0';
+
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
 	if (n_wrote == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
+	free(buffer);
 	close(fd);
 
 	return (n_read);
