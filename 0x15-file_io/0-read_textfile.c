2fa224e7d5bc75cb01a4aafe961d79223c188881
lumanya <shaibulumanya@gmail.com>
2022-07-27 17:26:53 +0300

    check for write condition

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,21 +16,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 
 	int fd;
-	ssize_t size;
+	ssize_t n_read, n_wrote;
 
 	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
-	size = read(fd, buffer, letters);
+	}
+	n_read = read(fd, buffer, letters);
 
-	write(STDOUT_FILENO, buffer, letters);
-
+	n_wrote = write(STDOUT_FILENO, buffer, letters);
+	if (n_wrote == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 	close(fd);
 
-	return (size);
+	return (n_read);
 }
